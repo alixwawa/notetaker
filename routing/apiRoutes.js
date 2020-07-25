a0e3e7391664f93f480dff441c7beefc7ffdd638
Alix Wawa <alixwawa@yahoo.com>
2020-07-25 14:09:19 -0500

    api routes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,23 +1,53 @@
-var fs = require("fs");
-var path = require('path')
-
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
 module.exports = function(app) {
 
-  var dbjson = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  var dbjsonTwo = JSON.stringify(fs.appendFile("./db/db.json", "utf8"));
-
     app.get("/api/notes", function(req, res) {
-      res.json(dbjson);
+       
+        res.json(data);
+
     });
+
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+
 
     app.post("/api/notes", function(req, res) {
-      dbjsonTwo.push(req.body);
-    });
-        
 
-   
+        let newNote = req.body;
+        let uniqueId = (data.length).toString();
+        console.log(uniqueId);
+        newNote.id = uniqueId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
+    
+    app.delete("/api/notes/:id", function(req, res) {
+
+        let noteId = req.params.id;
+        let newId = 0;
+        console.log(`Deleting note with id ${noteId}`);
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
 
 }
-
