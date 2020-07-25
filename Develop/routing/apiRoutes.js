df1b416dd97fafbef9ffff7604abda03e3e1919a
Alix Wawa <alixwawa@yahoo.com>
2020-07-25 13:08:46 -0500

    trying to get heroku to work

--- Develop/routing/apiRoutes.js
+++ Develop/routing/apiRoutes.js
@@ -6,14 +6,14 @@ var path = require('path')
 module.exports = function(app) {
 
   var dbjson = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  JSON.stringify(fs.appendFile("./db/db.json", "utf8"));
+  var dbjsonTwo = JSON.stringify(fs.appendFile("./db/db.json", "utf8"));
 
     app.get("/api/notes", function(req, res) {
       res.json(dbjson);
     });
 
     app.post("/api/notes", function(req, res) {
-      dbjson.push(req.body);
+      dbjsonTwo.push(req.body);
     });
         
 
